8b073e967faa881bdf034a04283c7303a7566dc6
HeikoRibberink <heiko.ribberink@deblijdeaankomst.nl>
2022-03-19 13:54:36 +0100

    Fixed bug with servo not utilising max steering angle.

--- rustycar/src/motor.rs
+++ rustycar/src/motor.rs
@@ -2,7 +2,9 @@ use rppal::gpio::{self, OutputPin};
 use std::time::Duration;
 
 pub fn motor(pin_f: &mut OutputPin, pin_b: &mut OutputPin, speed: f64) -> gpio::Result<()> {
-	if speed.abs() > 1.0 {panic!("Speed ({}) is outside of range [-1, 1]", speed)}
+	if speed.abs() > 1.0 {
+		panic!("Speed ({}) is outside of range [-1, 1]", speed)
+	}
 	const MOTOR_FREQUENCY: f64 = 100.0;
 	if speed == 0.0 {
 		pin_f.clear_pwm()?;
@@ -18,28 +20,25 @@ pub fn motor(pin_f: &mut OutputPin, pin_b: &mut OutputPin, speed: f64) -> gpio::
 }
 
 pub fn motor_speed_to_duty(num: f64) -> f64 {
-	const ROUND: f64 = 10.0;
+	const ROUND: f64 = 0.03; //Should be more than 0.
 	const MIN_DUTY: f64 = 0.22;
-	if num < ROUND && num > -ROUND {
+	if num.abs() < ROUND {
 		return 0.0;
 	}
-	if num == 0.0 {
-		return 0.0;
-	}
-	(num / std::i8::MAX as f64) * (1.0 - MIN_DUTY) + (MIN_DUTY * num.signum())
+	num * (1.0 - MIN_DUTY) + (MIN_DUTY * num.signum()) // Lerp
 }
 
 pub fn servo(pin: &mut OutputPin, angle: f64) -> gpio::Result<()> {
-	use std::f64::consts::{PI, FRAC_1_PI};
+	use std::f64::consts::{FRAC_2_PI, PI};
+	//Angle from [-90*; 90*] = [-PI/2; PI/2]
 	const MIN_ANGLE: f64 = -0.5 * PI;
 	const MAX_ANGLE: f64 = 0.5 * PI;
-	
 	if angle < MIN_ANGLE || angle > MAX_ANGLE {
 		panic!("Steering angle {} is outside of range [-PI/2; PI/2]", angle);
 	}
 	pin.set_pwm(
 		Duration::from_millis(20),
-		Duration::from_micros((500.0 + 2000.0 * angle * FRAC_1_PI * 0.5) as u64),
+		Duration::from_micros((1500.0 + 1000.0 * angle * FRAC_2_PI) as u64),
 	)?;
 	Ok(())
 }
